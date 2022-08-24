472c61cdf402180eb0ca6769a6ca6ead5080f476
Silke Blansjaar <silkeblansjaar@gmail.com>
2022-08-24 15:44:42 +0100

    Installed Axios and integrated API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   
   let [searchword, setSearchword] = useState("");
 
+  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchword}`;
+//API documentation: https://dictionaryapi.dev/
+
+  function handleResponse(response) {
+    console.log(response);
+  }
 
   function search(event) {
     event.preventDefault();
-    alert(`searching for ${searchword}`)
+    alert(`searching for ${searchword}`);
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleWordChange(event) {
